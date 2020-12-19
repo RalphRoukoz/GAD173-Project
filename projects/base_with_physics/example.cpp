6de4a3f2b3f22b3f0f936944e36b54037e489895
Roukoz <ralphroukoz@gmail.com>
2020-12-19 15:00:14 +0200

    Cleaned up code

--- projects/base_with_physics/example.cpp
+++ projects/base_with_physics/example.cpp
@@ -23,24 +23,6 @@ void Example::update(float deltaT)
 		m_running = false;
 	}
 
-	ImGui::Begin("Kage2D");
-	if(ImGui::Button("Start Game"))
-	{ 
-		sceneManager.Run(2);
-	}
-	if (ImGui::Button("Option"))
-	{
-		sceneManager.Run(1);
-	}
-	if (ImGui::Button("Back To Menu"))
-	{
-		sceneManager.Run(0);
-	}
-	if (ImGui::Button("Exit"))
-	{
-		m_running = false;
-	}
-	ImGui::End();
 
 	sceneManager.Update(deltaT);
 }
